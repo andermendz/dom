01145161981b1d9bcd53e6cfab6d98c3adcdd82c
andermendz <andersonmendz19@gmail.com>
2023-01-03 19:06:21 -0500

    came back with better logic, understanding better

--- script.js
+++ script.js
@@ -1,55 +1,52 @@
-const mydiv = document.querySelector("#mydiv");
+console.log("working");
+
+const container = document.querySelector("#container");
+const body = document.querySelector("body");
+
 const content = document.createElement("div");
 content.classList.add("content");
-content.textContent = "This is my content";
+content.textContent = "It's Working";
+container.appendChild(content);
 
-mydiv.appendChild(content);
+const thered = document.createElement("p");
+thered.textContent = "I'm the red";
+thered.setAttribute("style", "color: red");
+body.appendChild(thered);
 
-const p1 = document.createElement("p");
-p1.textContent = "Hey i'm red";
-p1.style.color = "red";
-mydiv.appendChild(p1);
+const blueh3 = document.createElement("h3");
+blueh3.textContent = "I'm the blue h3";
+blueh3.setAttribute("style", "color: blue");
+body.appendChild(blueh3);
 
-const myh3 = document.createElement("h3");
-myh3.textContent = "I'm a blue H3";
-myh3.setAttribute("style", "color: blue");
+const father = document.createElement("div");
+father.setAttribute(
+  "style",
+  "border: 3px solid black",
+  "background-color: pink",
+  "border-radius: 30px"
+);
 
-mydiv.appendChild(myh3);
+const dh1 = document.createElement("h1");
+dh1.textContent = "I'm in a div";
+father.appendChild(dh1);
 
-const newD = document.createElement("div");
-newD.textContent = "testing";
-newD.setAttribute("style", "border: 1px solid black; background-color:pink");
+const dp = document.createElement("p");
+dp.textContent = "Me too";
+father.appendChild(dp);
 
-const myh1 = document.createElement("h1");
-myh1.textContent = "im a div";
-newD.appendChild(myh1);
+const btn = document.createElement("button");
+btn.textContent = "Click Me";
+father.appendChild(btn);
 
-const p2 = document.createElement("p");
-p2.textContent = "ME TOO";
-newD.appendChild(p2);
-
-mydiv.appendChild(newD);
-
-const m2 = document.querySelector("#m2");
-m2.onclick = () => alert("metodo 2 pto");
-
-const m3 = document.querySelector("#m3");
-
-m3.addEventListener("click", () => {
-  alert("metodo 3 pto");
+var clicked = 0;
+btn.addEventListener("click", () => {
+  console.log("clicked");
+  clicked++;
+  counter.textContent = `I've been clicked ${clicked} times`;
 });
 
-const mf = document.querySelector("#mf");
-mf.addEventListener("click", extraMethod);
+const counter = document.createElement("div");
+counter.textContent = `I've been clicked ${clicked} times`;
+father.appendChild(counter);
 
-function extraMethod() {
-  alert("You clicked a button");
-}
-
-mf.addEventListener("click", function (e) {
-  console.log(e);
-});
-
-mf.addEventListener("click", function (e) {
-  e.target.style.background = "blue";
-});
+body.appendChild(father);
