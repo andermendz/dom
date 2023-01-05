1212e5f7ab24d70cacc522b081b5f49438587cd2
andermendz <andersonmendz19@gmail.com>
2023-01-05 18:48:08 -0500

    done

--- practice 6 mouse tracking/script.js
+++ practice 6 mouse tracking/script.js
@@ -1,10 +1,10 @@
-const trail = document.querySelector('.trail')
-window.addEventListener('mousemove',(e)=>{
-
-    console.log(`Mouse is at X ${e.pageX} and Y ${e.pageY}`);
-    trail.setAttribute('style',`display:flex; margin-left: ${e.pageX}px;
-                                             margin-top: ${e.pageY}px;
-`)
-
-
-})
+const trail = document.querySelector(".trail");
+window.addEventListener("mousemove", (e) => {
+  console.log(`Mouse is at X ${e.pageX} and Y ${e.pageY}`);
+  trail.setAttribute(
+    "style",
+    `display:flex; margin-left: ${e.pageX - 50}px;
+                   margin-top: ${e.pageY - 50}px;
+`
+  );
+});
